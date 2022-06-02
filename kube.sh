0f099132b1c513623eb832be792ebf4e16ee1c95
alexgrek <alexgrek.hq@gmail.com>
2022-06-02 14:38:51 +0300

    added kube nstall

---
+++ kube.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt update
+apt install -y docker.io curl vim
+systemctl enable --now docker
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+apt install -y kubeadm kubelet kubectl
+
+swapoff -a
+echo "All installed!"
+echo "vim /etc/fstab"
