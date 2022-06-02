b104c6ac09e647469d87e57414c667783bfd1c4e
alexgrek <alexgrek.hq@gmail.com>
2022-06-02 17:45:40 +0300

    versions

--- kube.sh
+++ kube.sh
@@ -6,7 +6,7 @@ systemctl enable --now docker
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add
 apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
-apt install -qy kubeadm=1.20 kubelet=1.20 kubectl=1.20
+apt install -qy kubeadm=1.20.6-00 kubelet=1.20.6-00 kubectl=1.20.6-00
 
 swapoff -a
 echo "All installed! Remove swap if exists:"
