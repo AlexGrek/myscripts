164650fdf8895fb38f45639ff69eea1e768f14fa
Alex Grek <AlexGrek.HQ@gmail.com>
2022-06-06 17:10:18 +0300

    resets

---
+++ reset/sudo_reset01.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubeadm reset -f
+
+rm -rf /etc/cni /etc/kubernetes /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/run/kubernetes ~/.kube/*
---
+++ reset/sudo_reset_iptables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+iptables -F && iptables -X
+iptables -t nat -F && iptables -t nat -X
+iptables -t raw -F && iptables -t raw -X
+iptables -t mangle -F && iptables -t mangle -X
